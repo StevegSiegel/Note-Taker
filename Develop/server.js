7abf5e3cb433618c523f6e8cd272e3aa7bb93913
Steve Siegel <stevegabrielsiegel@gmail.com>
2021-06-29 22:45:12 -0500

    fixed some typos, and did some reformating

--- Develop/server.js
+++ Develop/server.js
@@ -1,46 +1,50 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// routes
 
-app.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('./db/db.json', function(err, data) {
-    fs.readFile('./db/db.json', function(err, data) {
-        console.log(data);
-        res.send(data);
-    });
-});
-
-app.post('./api/notes', function(req, res) {
-    fs.readFile('./db/db.json', function(err, data) {
-        console.log(data);
-        const addNote = req.body;
-        addNote.id = uuidv4();
-        const note = JSON.parse(data);
-        note.push(addNote);
-        fs.writeFile('.db/db.json', JSON.stringify(note), function() {
-            res.send(addNote);
-        });
-    });
-});
-
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.use(express.urlencoded({extended: true}));
-app.use(expres.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 
-app.listen(PORT, function() {
-    console.log(`listening on PORT: ${PORT}`);
+
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+app.get('/api/notes', function (req, res) {
+    fs.readFile('./db/db.json', 'utf8', (error, data) => {
+        console.log(data)
+        res.send(data)
+    })
+});
+
+app.post('/api/notes', function (req, res) {
+    fs.readFile('./db/db.json', 'utf8', (error, data) => {
+        console.log(data)
+        const newNote = req.body
+        newNote.id = uuidv4()
+        const notes = JSON.parse(data)
+        notes.push(newNote)
+        fs.writeFile('./db/db.json', JSON.stringify(notes), () => {
+            res.send(newNote);
+        })
+    })
+})
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+
+
+app.listen(PORT, function () {
+
+    console.log(`App listening on PORT ${PORT}`)
 });
 
