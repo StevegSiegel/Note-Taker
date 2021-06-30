6dfa4fe17378018b1a71dcc7b219ab6bb1842d17
Steve Siegel <stevegabrielsiegel@gmail.com>
2021-06-29 21:21:14 -0500

    added get routes

--- Develop/server.js
+++ Develop/server.js
@@ -5,3 +5,17 @@ const {v4: uuidv4} = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// routes
+
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('./db/db.json', function(err, data) {
+    fs.readFile('./db/db.json', function(err, data) {
+        console.log(data);
+        res.send(data);
+    });
+});
+
